bb8ae08c3cfb8fc6633ef27b94a128fd3c4cad74
meng-yanchen <mengyanchen827@gmail.com>
2025-12-10 21:23:53 -0500

    Completed alex day 5 part 1

--- alex/day5/part1.py
+++ alex/day5/part1.py
@@ -11,6 +11,32 @@ input = [
     "17",
     "32",
 ]
-# with open('input.txt', 'r') as file:
-#     input = file.read()
-# input = input.split('\n')
+with open('input.txt', 'r') as file:
+    input = file.read().split('\n')
+
+freshIngredientIdRanges = []
+availableIds = []
+
+readFirstPart = True
+for line in input:
+    if readFirstPart:
+        if line == "":
+            readFirstPart = False
+            continue
+        else:
+            freshIngredientIdRanges.append(line)
+    else:
+        availableIds.append(int(line))
+
+totalFreshCount = 0
+
+for id in availableIds:
+    for r in freshIngredientIdRanges:
+        rangeStart = int(r.split("-")[0])
+        rangeEnd = int(r.split("-")[1])
+
+        if id >= rangeStart and id <= rangeEnd:
+            totalFreshCount += 1
+            break
+
+print(f"answer: {totalFreshCount}")
